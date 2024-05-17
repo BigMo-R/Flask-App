59757cba1b8481b99074593e21137131526d1423
BigMo-R <148564801+BigMo-R@users.noreply.github.com>
2024-05-16 21:07:45 -0400

    Update gunicorn-cfg.py

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,11 +1,24 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
+# gunicorn-cfg.py
 
-bind = '0.0.0.0:5005'
-workers = 1
-accesslog = '-'
-loglevel = 'debug'
-capture_output = True
-enable_stdio_inheritance = True
+import multiprocessing
+import os
+
+# Get the port from the environment variable PORT (set by Render)
+port = os.getenv('PORT', '8000')
+
+# Basic settings
+bind = f"0.0.0.0:{port}"
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = 2  # Number of threads per worker
+timeout = 120  # Time in seconds before a worker is killed and restarted
+
+# Logging
+accesslog = '-'  # Log access to stdout
+errorlog = '-'  # Log errors to stdout
+loglevel = 'info'  # Set the log level to info
+
+# Graceful timeout
+graceful_timeout = 120
+
+# Keep-alive settings
+keepalive = 5
